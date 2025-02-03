193594a9b12693cf81acdd563546a42db5c7502e
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-02-03 09:05:48 +0900

    Bridge

---
+++ src/bridgeTest/CountDisplay.java
@@ -0,0 +1,16 @@
+package bridgeTest;
+
+public class CountDisplay extends Display {
+
+    public CountDisplay(DisplayImpl impl) {
+        super(impl);
+    }
+
+    public void multiDisplay(int time) {
+        open();
+        for(int i=0; i<time; i++) {
+            print();
+        }
+        close();
+    }
+}
---
+++ src/bridgeTest/Main.java
@@ -0,0 +1,14 @@
+package bridgeTest;
+
+public class Main {
+    public static void main(String[] args) {
+        Display d1 = new Display(new StringDisplayImpl("Hello, Korea"));
+        Display d2 = new Display(new StringDisplayImpl("Hello, World"));
+        CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, World"));
+
+        d1.display();
+        d2.display();
+        d3.display();
+        d3.multiDisplay(5);
+    }
+}
